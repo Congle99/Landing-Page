18403ac74ecb741ef1c09418093387e916c8ea33
Lê Xuân Công <91440559+Congle99@users.noreply.github.com>
2026-07-04 16:36:55 +0700

    test bot

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import { SYSTEM_PROMPT } from "../data/chatbotPrompt";
-
+console.log(import.meta.env.VITE_GEMINI_API_KEY);
 const ai = new GoogleGenAI({
   apiKey: import.meta.env.VITE_GEMINI_API_KEY,
 });
